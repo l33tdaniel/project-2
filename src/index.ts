1ec580d226cc565dffcfb2092837de1d1469bd6e
jannadungao <jannadungao2@gmail.com>
2025-08-30 21:28:12 -0500

    Use user input for mine count

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 console.log("Hello!");
 
 import { generateMinefield } from "./minefield_gen";
-const minefield = generateMinefield({ row: 3, col: 5 }, 10, 10, 1, 10);
-console.table(minefield);
+import { getMineCount } from "./userMineCount";
+
+let mineField: number[][];
+
+document.addEventListener('DOMContentLoaded', () => {
+    getMineCount((mineCount: number) => {
+        console.log(`User set mine count: ${mineCount}`);
+
+        // Generate minefield
+        mineField = generateMinefield({ row: 0, col: 0 }, 10, 10, 1, mineCount);
+        console.table(mineField);
+        console.log(`Minefield generated.`);
+    });    
+});
+
+
+// const minefield = generateMinefield({ row: 3, col: 5 }, 10, 10, 1, 10);
+// console.table(minefield);

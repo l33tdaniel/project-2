985546e49c41e86abf1dc88e7003785404edc1a3
jannadungao <jannadungao2@gmail.com>
2025-08-30 22:25:47 -0500

    Display remaining mine count
    Created custom event at end of userMineCount.ts to handle updating the count when a flag is placed or removed. Event listener should be added to flag handling file

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,5 @@ document.addEventListener('DOMContentLoaded', () => {
     });    
 });
 
-
 // const minefield = generateMinefield({ row: 3, col: 5 }, 10, 10, 1, 10);
 // console.table(minefield);
--- src/userMineCount.ts
+++ src/userMineCount.ts
@@ -1,6 +1,6 @@
 // get user specified mine count for minefield
 let mineCount: number = 10;
-
+let minesRemaining: number = 10;
 /**
  * 
  * @param countSet - callback function called after minecount is entered by user
@@ -27,7 +27,7 @@ export function getMineCount(countSet: (mineCount: number) => void) {
             countInput.focus();
             return;
         }
-
+        updateDisplay();
         countSet(mineCount);
 
         console.log(`Mine count set: ${mineCount}`);
@@ -39,6 +39,45 @@ export function getMineCount(countSet: (mineCount: number) => void) {
  * @returns current mine count
  */
 
-export function getCurrentMineCount(): number {
+export function getMinesRemaining(): number {
     return mineCount;
 }
+
+/**
+ * 
+ * @param change - +1 when user removes a flag, -1 when user adds a flag
+ */
+
+export function updateRemaining(change: number): void {
+    minesRemaining += change;
+    updateDisplay();
+}
+
+/**
+ * Update count for html
+ */
+export function updateDisplay(): void {
+    const mineDisplay = document.getElementById('minesRemaining');
+    if (mineDisplay) {
+        mineDisplay.textContent = minesRemaining.toString();
+        console.log(`Count updated.`);
+    }
+}
+
+/**
+ * listen for when flag added/removed
+ */
+document.addEventListener('flagPlaced', (event: Event) => {
+    const customEvent = event as CustomEvent<{ change: number }>;
+    updateRemaining(customEvent.detail.change);
+});
+
+// for flag handling file*
+/*
+if placing a flag 
+{
+    document.dispatchEvent(new CustomEvent('flagPlaced', { detail: { change: -1 } }));
+} else if removing a flag {
+    document.dispatchEvent(new CustomEvent('flagPlaced', { detail: { change: 1 } }));
+}
+*/

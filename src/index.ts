05cc20477c8f2681f66e0ab0b1c3814a1591031b
Iwantexpresso <marcosestrella2011@gmail.com>
2025-09-02 17:24:27 -0500

    commented out testing matrix

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ window.addEventListener("DOMContentLoaded", () => {
     startGame();
 });
 
-const minefield = generateMinefield({ row: 3, col: 5 }, 10, 10, 1, 10);
-console.table(minefield);
+//const minefield = generateMinefield({ row: 3, col: 5 }, 10, 10, 1, 10);
+//console.table(minefield);

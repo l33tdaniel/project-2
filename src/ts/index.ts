5f090750dcedb44158e07a0bda203df165339b28
Iwantexpresso <marcosestrella2011@gmail.com>
2025-09-09 18:53:21 -0500

    added row header functionality to page

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -6,10 +6,15 @@ import { fillGrid } from "./create_grid";
 import { setFlaggingHandlers } from "./flagging";
 import { generateMinefield } from "./minefield_gen";
 import { startGame } from "./reveal";
+import { addLabels } from "./create_grid";
 
 window.onload = () => {
+    const rows = 10;
+    const cols = 10;
     const grid: HTMLDivElement = document.querySelector("div#grid")!;
-    const tileMatrix = fillGrid(grid);
+    const tileMatrix = fillGrid(grid,rows,cols);
+    const gridheaders = addLabels(rows,cols);
+
     setFlaggingHandlers(tileMatrix);
     startGame();
 };

1ca890acd32b19fe83c92e07eba138f87fdf0d4a
jannadungao <jannadungao2@gmail.com>
2025-09-17 09:34:29 -0500

    Add comments

--- src/ts/status.ts
+++ src/ts/status.ts
@@ -1,15 +1,18 @@
+// display playing status once game starts
+// function is called in startGame()
 export function showPlayingStatus() {
     const statusDiv = document.getElementById('statusIndicator');
     const statusText = document.getElementById('statusText');
     if (statusDiv && statusText) {
-        statusText.textContent = "Playing";
+        statusText.textContent = "Playing"; // to appear on screen when playing
         statusDiv.style.display = "block";
     }
 }
 
+// hidden before game start and after game fail/win
 export function hideStatus() {
     const statusDiv = document.getElementById('statusIndicator');
     if (statusDiv) {
-        statusDiv.style.display = "none";
+        statusDiv.style.display = "none"; // hides display
     }
 }
